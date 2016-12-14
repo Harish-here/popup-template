a0427308df44f0cf4ee64d60a954d2c5a14b0ff6
Harish-here <harishamudha@gmail.com>
2016-12-14 23:57:56 +0530

    Modified to use underscore.js templates

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $(function(){
- //initiate
+ //initiate popup
 $(window).on('load',function(){
  $('.popup').css('right','20px');
 })
@@ -8,16 +8,67 @@ $(window).on('load',function(){
 $('.close').click(function(){
   $('.popup').css('right','-420px');//$('.popup').hide() can be used to avoid animation
 })
-var path="images/";
-//to chANGE image
-$('button').on('click',function(){
-  srcs1 = path+this.id+"1.jpg";//getting the id to set path to  image/{id}1.jpg
-  srcs2 = path+this.id+"2.jpg";
-  srcs3 = path+this.id+"3.jpg";
-  $("#1").attr("src",srcs1); $("#4").attr("src",srcs1);
-  $("#2").attr("src",srcs2);
-  $("#3").attr("src",srcs3);
 
+
+//making the button template
+var button_id = ['h','n','b','r','g','p'],
+buttonTemplate = _.template("<button id='<%= btn %>' class='btn btn-default'>\"<%= btn %>\" set</button>&nbsp;"),
+content ='';
+//iterate to make all buttons
+_.each(button_id,function(btn,index,button_id){
+content += buttonTemplate({
+  btn : btn
+});
+});
+
+$("#firstButton").html(content);//end of buttonTemplate
+
+//popup template 
+noImage=['1','2','3','4'];
+const startTag = "<div class='col-md-6'>",endTag = "</div>",
+labelContent = "<div class='label_c pull-left'><h5><b>Cotton white Dresses</b></h5>see more</div>";
+imageTemplate = _.template("<img id='<%= u %>' src='images/r<%= imgNo %>.jpg' class='img-responsive pull-left img-width'>");
+imageContent =""+startTag;
+
+//iterate to make hte imageContent
+_.each(noImage,function(imgNo,index,noImage){
+  if(imgNo == 3){
+imageContent += labelContent + endTag + startTag;
+  }
+  if(imgNo == 4){
+  imgNo = 1;//since there is no fourth image,sets to 1
+}
+imageContent += imageTemplate({
+imgNo : imgNo,
+u : index + 1 //sets the id for the image
 })
+
+});//end of iteration
+
+imageContent += labelContent + endTag;//finishing the content
+
+$('.popup-area').html(imageContent);//end of popupTemplate
+
+var path="images/";
+// template for p1
+const pContent = "<div class='col-md-6 col-md-offset-3'><img class='img-responsive pull-left' src='"+path+"p1.jpg' >"+ labelContent +endTag;
+
+
+//to change image dynamically
+$('button').on('click',function(){
+  if(this.id == 'p'){
+    $('.popup-area').html(pContent);
+  }else{
+    $('.popup-area').html(imageContent); //to remove the pContent if it is there
+      //changing the path of the image
+      for (i = 0; i <= noImage.length; i++) {
+          if(i != 4) 
+          $("#"+i+"").attr('src',path+this.id+i+".jpg");
+          else
+          $("#"+i+"").attr('src',path+this.id+"1.jpg");
+        }
+  }
+ 
+});
 
 })
